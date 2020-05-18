baf91b809bf310db81bd8f2fed2c60d388aa8600
kamizaneyae <kamizaneyae@192.168.3.13>
2020-05-19 01:07:22 +0900

    hello

---
+++ sw.kadai1/src/sw/kadai1/food.java
@@ -0,0 +1,14 @@
+package sw.kadai1;
+
+public class food {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("チーズ");
+		System.out.println("色々な料理に使える");
+		System.out.println("そのままでもおいしい");
+		
+	}
+
+}
