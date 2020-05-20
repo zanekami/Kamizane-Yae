dc2da198b3cb95388120abd99177428c2c02e3cc
kamizaneyae <kamizaneyae@192.168.3.13>
2020-05-21 00:40:39 +0900

    変更

--- sw.kadai1/src/sw/kadai1/food.java
+++ sw.kadai1/src/sw/kadai1/food.java
@@ -5,7 +5,7 @@ public class food {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("チーズ");
+		System.out.println("好きな食べ物はチーズです");
 		System.out.println("特にクリームチーズが好きです");
 		System.out.println("色々な料理に使るし、そのままでも美味しいので好きです");
 		
